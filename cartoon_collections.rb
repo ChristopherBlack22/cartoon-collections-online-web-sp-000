28472d8c2512ea2d3c420ab354ef5f63db2e08bf
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-05 23:33:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? {|call| call.length >4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  array.include?(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
