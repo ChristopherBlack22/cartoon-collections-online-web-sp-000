8bed13ccf6972567ebce7695448c4242b8eff475
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-05 23:23:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,12 +10,11 @@ end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |call|
     call.capitalize << "!"
-   # binding.pry 
   end 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls) 
+  planeteer_calls.
 end
 
 def find_the_cheese# code an argument here
